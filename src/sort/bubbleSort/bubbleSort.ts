03b6fd3925fd319bbc78993321dd37a0af86f9a8
aimee goddard <aimeegoddard@aimees-MacBook-Pro.local>
2021-12-08 12:13:19 +0000

    fourth

--- src/sort/bubbleSort/bubbleSort.ts
+++ src/sort/bubbleSort/bubbleSort.ts
@@ -2,13 +2,20 @@ import { SortAlgorithm } from "../common";
 import swap from "../swap";
 
 const bubbleSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
-    let list_array = items; {
-        for(let i = items.length-1; i>1; i--) {
-            let swap = false 
-        }
-        for ( in (items.length-1)) 
-    }
+    let output_list = items;
+    for(let i = (items.length); i > 0; i--){
+        let swap_made:boolean = false
 
+        for (let j = 0; j<items.length-1; j++){
+            if(output_list[j] > output_list[j+1]){
+                swap_made= true
+                swap(output_list, j, j+1)
+            }
+        }
+        if (swap_made = false){
+            break
+        }
+    }
     return items;
 }
 
